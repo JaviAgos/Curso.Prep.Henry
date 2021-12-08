749c1131edcd01b27df644594a18cad4b31b3d78
Kenzio21 <javiagostino18@gmail.com>
2021-12-08 13:29:31 -0300

    solution6

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -192,7 +192,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  var str = "Hola";
+  var str
   var nuevaStr = str + "!";
   return nuevaStr;
 }
