31c9866bb4e17a0eb3a847c792db34d9276dc063
Kenzio21 <javiagostino18@gmail.com>
2021-12-08 13:36:28 -0300

    solution10

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -244,8 +244,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var euro = 1;
-  var dolar = euro / 1.20;
+  var dolar = euro * 1.20;
   return dolar;
 }
 
